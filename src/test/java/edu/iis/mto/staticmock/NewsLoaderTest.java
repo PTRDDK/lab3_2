92a64a96636ab3ea863b53d24159d514f9142fc0
Piotr Dudek <194936@edu.p.lodz.pl>
2017-04-03 21:13:56 +0200

    checkIfPublicNewsAreAddedToPublishForOneNewsTest test -> ADD

--- src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -2,11 +2,17 @@ package edu.iis.mto.staticmock;
 
 import edu.iis.mto.staticmock.reader.WebServiceNewsReader;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
@@ -32,6 +38,18 @@ public class NewsLoaderTest {
         webServiceNewsReader = Mockito.mock(WebServiceNewsReader.class);
         mockStatic(NewsReaderFactory.class);
         NewsReaderFactory newsReaderFactory = Mockito.mock(NewsReaderFactory.class);
-        Mockito.when(NewsReaderFactory.getReader(configuration.getReaderType())).thenReturn(webServiceNewsReader);
+        Mockito.when(newsReaderFactory.getReader(configuration.getReaderType())).thenReturn(webServiceNewsReader);
+    }
+
+    @Test
+    public void checkIfPublicNewsAreAddedToPublishForOneNewsTest() throws Exception{
+        IncomingNews incomingNews = new IncomingNews();
+        incomingNews.add(new IncomingInfo("test test test", SubsciptionType.NONE));
+        Mockito.when(webServiceNewsReader.read()).thenReturn(incomingNews);
+        NewsLoader newsLoader = new NewsLoader();
+        PublishableNews publishableNews = newsLoader.loadNews();
+        List<String> result = (List<String>) Whitebox.getInternalState(publishableNews, "publicContent");
+        assertThat(result.size(), is(equalTo(1)));
+        assertThat(result.get(0), is(equalTo("test test test")));
     }
 }
