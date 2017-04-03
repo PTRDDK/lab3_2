da937d9e38e6de4e31463dbe9f168070508e1101
Piotr Dudek <194936@edu.p.lodz.pl>
2017-04-03 21:05:07 +0200

    setUp method -> ADD

--- src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -1,10 +1,37 @@
 package edu.iis.mto.staticmock;
 
+import edu.iis.mto.staticmock.reader.WebServiceNewsReader;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
 import static org.junit.Assert.*;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
 /**
  * Created by Piotrek on 03.04.2017.
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ConfigurationLoader.class, NewsReaderFactory.class})
 public class NewsLoaderTest {
 
+    private WebServiceNewsReader webServiceNewsReader;
+
+    @Before
+    public void setUp(){
+        Configuration configuration = Mockito.mock(Configuration.class);
+        Mockito.when(configuration.getReaderType()).thenReturn("WS");
+
+        mockStatic(ConfigurationLoader.class);
+        ConfigurationLoader configurationLoader = Mockito.mock(ConfigurationLoader.class);
+        Mockito.when(ConfigurationLoader.getInstance()).thenReturn(configurationLoader);
+        Mockito.when(configurationLoader.loadConfiguration()).thenReturn(configuration);
+
+        webServiceNewsReader = Mockito.mock(WebServiceNewsReader.class);
+        mockStatic(NewsReaderFactory.class);
+        NewsReaderFactory newsReaderFactory = Mockito.mock(NewsReaderFactory.class);
+        Mockito.when(NewsReaderFactory.getReader(configuration.getReaderType())).thenReturn(webServiceNewsReader);
+    }
 }
